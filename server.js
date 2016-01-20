5010a9208adc8e99fa348bb7999dd5c3b9a21cb2
Yazan <ylpizra@my.bridgeport.edu>
2016-01-20 17:15:38 -0500

    fixed server.js router issue

--- server.js
+++ server.js
@@ -14,8 +14,14 @@ app.use(bodyParser.json());
 
 //Routes for API ===============================================================
 
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/js', express.static(__dirname + '/public/js'));
+app.use('/img', express.static(__dirname + '/public/img'));
+app.use('/font-awesome', express.static(__dirname + '/public/font-awesome'));
+app.use('/fonts', express.static(__dirname + '/public/fonts'));
+
 app.get('/', function(req, res) {
-    res.sendFile('./public/index.html');
+    res.sendFile(__dirname+'/public/index.html');
 });
 
 app.listen(port);
